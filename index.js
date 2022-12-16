7a0abac70b17ffa2ddc5bcbc9913e630a59b76d8
Jabo Jean Marie <97763818+Heisjabo@users.noreply.github.com>
2022-12-16 17:14:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+
+console.log(saveEl);
+
+let count = 0;
+
+function increment() {
+    count += 1;
+
+    countEl.textContent = count;
+    
+}
+function save() {
+    let countStr = count+ " - ";
+
+    saveEl.textContent += countStr;
+    console.log(count);
+    countEl.textContent = 0;
+    count = 0;
+
+}
